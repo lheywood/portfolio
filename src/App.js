7fc0679de7361080ba0fb1e468431875c0e56a65
lheywood <liamheywood@weareimmutable.com>
2019-02-28 23:35:54 -0600

    landing page

--- src/App.js
+++ src/App.js
@@ -2,31 +2,32 @@ import React, { Component } from 'react';
 import './App.scss';
 import  { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 import Main from './components/main';
+import { Link } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="navContent" style={{height: '300px', position: 'relative'}}>
+      <div className="mainPage">
     <Layout>
         <Header className="header-color" title={<span><a id="homeLink" href="landingPage">Liam Heywood /<strong>Online Portfolio</strong></a></span>}>
             <Navigation>
-                <a href="AboutMe">About Me</a>
-                <a href="Resume">Resume</a>
-                <a href="Projects">Projects</a>
-                <a href="Contact">Contact Us</a>
+                <Link to="AboutMe">About Me</Link>
+                <Link to="Resume">Resume</Link>
+                <Link to="Projects">Projects</Link>
+                <Link to="Contact">Contact Us</Link>
             </Navigation>
         </Header>
         <Drawer title="Title">
             <Navigation>
-                <a href="AboutMe">About Me</a>
-                <a href="Resume">Resume</a>
-                <a href="Projects">Projects</a>
-                <a href="Contact">Contact Us</a>
+                <Link to="AboutMe">About Me</Link>
+                <Link to="Resume">Resume</Link>
+                <Link to="Projects">Projects</Link>
+                <Link to="Contact">Contact</Link>
             </Navigation>
         </Drawer>
         <Content>
-           <div >  
+           <div className="landingGrid">  
             <Main />
               
             </div>
--- src/components/landingPage.js
+++ src/components/landingPage.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
-import img from './imgs/one.jpg';
+import  ukimg  from './imgs/uk.png';
+import usimg from './imgs/usa.png';
 import { Grid, Cell } from 'react-mdl';
+
 
 class LandingPage extends Component {
     render() {
         return(
-           
-            <p>Landing LandingPage</p>
+           <div style={{width: '100%', margin:'auto'}}>
+           <Grid className="landingGrid">
+                <Cell col={12}>
+                
+                <img src={ukimg} alt="uk" className="homeImg"/>
+                <img  src={usimg} alt="uk" className="homeImg"/>
+
+                <div className="bannerText">
+                <h1>
+                    Graduate of Computer Science & Mathematics minor
+                </h1>
+                <br/>
+                <p>Experience with:</p>
+                <p>Java | JavaScript | NodeJS | React | HTML/CSS | C# </p>
+                </div>
+                
+                </Cell>
+            </Grid>
+
+
+           </div>
             
         )
     }
